cd80a13086d73333400d9204206a3761328188eb
ryan m <contact@ryanmeek.com>
2019-07-08 23:58:38 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,10 +1,9 @@
 module Players
   class Computer < Player
     def move(board)
-      opponent = self.token == "X" ? "O" : "X"
       Game::WIN_COMBINATIONS.each do |combo|
         progress = [board.cells[combo[0]], board.cells[combo[1]], board.cells[combo[2]]]
-        if progress.count(opponent) == 2 || progress.count(self.token) == 2
+        if progress.count("X") == 2 || progress.count("O") == 2
           combo.each do |cell|
             return cell + 1 if !board.taken?(cell + 1)
           end
