8cb2c33bfb5cee586f96c5526516c97f6fbdcb64
ryan m <contact@ryanmeek.com>
2019-07-08 15:37:38 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,22 +1,17 @@
 module Players
   class Computer < Player
-
     def move(board)
       opponent = self.token == "X" ? "O" : "X"
       Game::WIN_COMBINATIONS.each do |combo|
         progress = [board.cells[combo[0]], board.cells[combo[1]], board.cells[combo[2]]]
         if progress.count(opponent) == 2 || progress.count(self.token) == 2
           combo.each do |cell|
-            if !board.taken?(cell + 1)
-              return cell + 1
-            end
+            return cell + 1 if !board.taken?(cell + 1)
           end
         end
       end
       [5,1,3,7,9,2,4,6,8].each do |cell|
-        if !board.taken?(cell)
-          return cell.to_s
-        end
+        return cell.to_s if !board.taken?(cell)
       end
     end
   end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,11 +1,7 @@
 module Players
   class Human < Player
     def move(board)
-      if board.turn_count == 0
-        board.display_key
-      else
-        board.display
-      end
+      board.turn_count == 0 ? board.display_key : board.display
       puts "\nPlayer #{self.token}, choose 1-9:"
       gets
     end
